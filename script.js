62c8cb15a6aec631a4ee30c69882149e95987468
John Hamlin <johnhamlin@gmail.com>
2022-11-18 11:50:08 -0500

    Complete Step 4: Display Total

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Add validation!');
+'use strict';
+
+const formContainer = document.querySelector('.container');
+const totalDiv = document.querySelector('#total');
+
+formContainer.addEventListener('submit', makeReservation);
+
+function makeReservation(event) {
+  event.preventDefault();
+  displayTotal(5);
+}
+
+function calculateTotal() {
+  // let days =
+}
+
+function displayTotal(total) {
+  const totalEl = document.createElement('p');
+  totalEl.innerText = `Your total is $${total}`;
+  totalDiv.appendChild(totalEl);
+}
