a998f392f747ec0184328bb5b058a9fb35fa807d
John Hamlin <johnhamlin@gmail.com>
2022-11-20 22:56:53 -0500

    Clean up code. Mainly return statement logic. Namely in validateForm, validateCreditCard, checkExpDate

--- script.js
+++ script.js
@@ -16,28 +16,6 @@ formContainer.addEventListener('change', e => {
 //  FUNCTIONS TO CALCULATE AND DISPLAY TOTAL
 //////////////////////////////////////////////////
 /**
- * It calculates the total price of a rental car based on the number of days and the start date
- * @returns The total price of the rental.
- */
-function calculateTotal() {
-  // Array for looking up daily prices using 0-based values for days of week starting with Sunday
-  const prices = [7, 5, 5, 5, 5, 5, 7];
-  // Get values from DOM and create Date object
-  let days = +document.querySelector('#days').value;
-
-  const date = getStartDate();
-  // Initialize total
-  let total = 0;
-  // Loop to calculate total
-  for (days; days > 0; days--) {
-    total += prices[date.getDay()];
-    // increment date
-    date.setDate(date.getDate() + 1);
-  }
-  return total;
-}
-
-/**
  * It takes the value of the start date input, and returns a Date object with the time zone offset
  * to EST
  * @returns A new Date object with the date and timezone specified by the user.
@@ -50,6 +28,27 @@ function getStartDate() {
   const dateInput = document.querySelector('#start-date').value;
 
   return new Date(`${dateInput}${TIME_ZONE}`);
+}
+
+/**
+ * It calculates the total price of a rental car based on the number of days and the start date
+ * @returns The total price of the rental.
+ */
+function calculateTotal() {
+  // Array for looking up daily prices using 0-based values for days of week starting with Sunday
+  const prices = [7, 5, 5, 5, 5, 5, 7];
+  // Get values from DOM and create Date object
+  let days = +document.querySelector('#days').value;
+  const date = getStartDate();
+
+  // Loop to calculate total
+  let total = 0;
+  for (days; days > 0; days--) {
+    total += prices[date.getDay()];
+    // increment date
+    date.setDate(date.getDate() + 1);
+  }
+  return total;
 }
 
 /**
@@ -70,26 +69,14 @@ function displayTotal(total) {
 // CREDIT CARD VALIDATION
 //////////////////////////////////////////////////
 /**
- * If the number is 16 digits long and passes the Luhn check, then it's valid
- * @param number - The credit card number
- * @returns A boolean value.
- */
-function validateCardNumber(number) {
-  const regex = /^[0-9]{16}$/;
-  if (!regex.test(number)) return false;
-
-  return luhnCheck(number);
-}
-
-/**
  * Runs the Luhn algorithm to validate a credit card number.
  * @param val - The credit card number to validate.
  * @returns boolean
  */
 function luhnCheck(val) {
-  var sum = 0;
-  for (var i = 0; i < val.length; i++) {
-    var intVal = parseInt(val.substr(i, 1));
+  let sum = 0;
+  for (let i = 0; i < val.length; i++) {
+    let intVal = parseInt(val.substr(i, 1));
     if (i % 2 === 0) {
       intVal *= 2;
       if (intVal > 9) {
@@ -99,6 +86,16 @@ function luhnCheck(val) {
     sum += intVal;
   }
   return sum % 10 === 0;
+}
+
+/**
+ * If the number is 16 digits long and passes the Luhn check, then it's valid
+ * @param number - The credit card number
+ * @returns A boolean value.
+ */
+function validateCardNumber(number) {
+  const regex = /^[0-9]{16}$/;
+  return regex.test(number) ? luhnCheck(number) : false;
 }
 
 //////////////////////////////////////////////////
@@ -126,9 +123,15 @@ function makeReservation(event) {
  */
 function validateForm(e) {
   const today = new Date();
-  const todayMonth = today.getMonth();
+  const todayMonth = today.getMonth() + 1; // Convert zero-based month
   const todayFullYear = today.getFullYear();
   const todayShortYear = todayFullYear % 100;
+
+  const pass = function () {
+    e.parentElement.classList.remove('input-invalid');
+    e.setCustomValidity('');
+    return true;
+  };
 
   const fail = function (errorMsg) {
     e.parentElement.classList.add('input-invalid');
@@ -137,23 +140,14 @@ function validateForm(e) {
     return false;
   };
 
-  const pass = function () {
-    e.parentElement.classList.remove('input-invalid');
-    e.setCustomValidity('');
-    return true;
-  };
-
   const checkExpDate = function (exp) {
     const expMonth = +exp.slice(0, 2);
     const expYear = +exp.slice(3);
     // Ensure date is in the future
-    if (
-      todayShortYear > expYear ||
-      (todayMonth >= expMonth && todayShortYear === expYear)
-    )
-      return false;
-
-    return true;
+    return (
+      todayShortYear < expYear ||
+      (todayShortYear === expYear && todayMonth <= expMonth)
+    );
   };
 
   // List of validation tests for each form item keyed by id
@@ -208,12 +202,7 @@ function validateForm(e) {
       return pass();
     },
   };
-  // Don't think this is needed anymore. Not relying on html required tags
-  // if (!e.checkValidity()) return fail('This field is required');
-  // else pass(); // maybe check for blank separately
 
   // If any of the tests failed, return false
-  if (!validators[e.id](e)) return false;
-
-  return true;
+  return validators[e.id](e);
 }
